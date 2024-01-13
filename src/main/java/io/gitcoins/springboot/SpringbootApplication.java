780742a54ba6f71b35c4ea85f024b0986f4baf5c
Gitcoins <gitcoins@outlook.com>
2024-01-13 23:34:18 +0800

    update

--- src/main/java/io/gitcoins/springboot/SpringbootApplication.java
+++ src/main/java/io/gitcoins/springboot/SpringbootApplication.java
@@ -1,13 +1,9 @@
 package io.gitcoins.springboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+
 	}
 
 }
--- src/test/java/io/gitcoins/springboot/SpringbootApplicationTests.java
+++ src/test/java/io/gitcoins/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,7 @@
 package io.gitcoins.springboot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class SpringbootApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
